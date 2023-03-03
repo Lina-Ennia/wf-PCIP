4bd4bf6aa2e24f785739a627ad6a9a3bc46b8d2a
Lina Ennia <92579850+Lina-Ennia@users.noreply.github.com>
2023-03-03 10:34:48 +0100

    Update 1_chimeric_target_host.sh

--- 1_chimeric_target_host.sh
+++ 1_chimeric_target_host.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# USAGE: sh 1_chimeric_target_host.sh 
 
+# Change to our working directory
+cd ~/wf-PCIP
+
+# Location of the human genome hg38
 GENOME=~/wf-PCIP/RefSeq/host/GRCh38.p14.fasta
+
+# Location of the virus reference sequence
 TARGET=~/wf-PCIP/RefSeq/target/NC_001802.1.fasta
 
+# Make directory to store chimeric sequence
 mkdir TARGETHOST_INDEX
+
+# Move to the output directory
 cd TARGETHOST_INDEX
 
+# Concatenate virus and human sequences
 cat $GENOME $TARGET > BTA_BLV.fa
 
+# Set variable for the chimeric sequence
 TARGETHOST=~/wf-PCIP/TARGETHOST_INDEX/BTA_BLV.fa
